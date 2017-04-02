879ad61739e70d8a244e42483f540c9098f1ba1e
freya0608 <freya001@yeah.net>
2017-04-02 18:03:40 +0800

    优化

--- handwriting.js
+++ handwriting.js
@@ -5,6 +5,11 @@ var canvasWidth = 600;
 var canvasHeight = canvasWidth;
 var isMouseDown = false;
 
+var lastLocation = {x:0,y:0};
+var lastTimestamp = 0;
+var lastLineWidth = -1;
+
+
 var canvas =document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
@@ -16,7 +21,10 @@ drawGrid();
 canvas.onmousedown = function (e) {
     e.preventDefault();
     isMouseDown = true;
-    console.log('mousedown')
+    console.log('mousedown');
+
+    lastLocation = windowToCanvas(e.clientX,e.clientY);
+    lastTimestamp = new Date().getTime();
 };
 
 canvas.onmouseup = function (e) {
@@ -33,9 +41,88 @@ canvas.onmouseout = function (e) {
 canvas.onmousemove = function (e) {
     e.preventDefault();
     if(isMouseDown){
-        console.log('mousemove')
+        console.log('mousemove');
+        var  curLocation = windowToCanvas(e.clientX,e.clientY);
+        var curTimestamp = new Date().getTime();
+        var s = calDistance(curLocation,lastLocation);
+        var t = curTimestamp - lastTimestamp;
+
+        var lineWidth = calLineWidth(t,s);
+
+        //lastLocation和curLocation之间绘制线段
+        context.beginPath();
+        context.moveTo(lastLocation.x, lastLocation.y);
+        context.lineTo(curLocation.x,  curLocation.y);
+
+        context.strokeStyle= 'black';
+        context.lineWidth = lineWidth;
+        context.lineCap = 'round';//笔画链接流畅
+        context.lineJoin = 'round';
+        context.stroke();
+
+        lastLocation = curLocation;
+        lastTimestamp = curTimestamp;
+        lastLineWidth = lineWidth;
     }
 };
+
+
+var maxLineWidth = 30;
+var minLineWidth = 1;
+var maxStrokeV = 10;
+var minStrokeV = 0.1;
+function calLineWidth(t, s) {
+    var v = s/t;
+    var resultLineWith;
+    if(v<minStrokeV){
+        resultLineWith = maxLineWidth;
+    }else if(v>maxStrokeV){
+        resultLineWith = minLineWidth;
+    }else {
+        resultLineWith = maxLineWidth-(v-minStrokeV)/(maxStrokeV-minStrokeV)*(maxLineWidth-minLineWidth)
+    }
+
+
+    if(lastLineWidth==-1){
+        return resultLineWith;
+    }
+
+    return lastLineWidth*2/3+ resultLineWith*1/3;
+}
+// function calLineWidth(t, s) {
+//     var v = s/t;
+//     var resultLineWith;
+//     if(v<0.1){
+//         resultLineWith = 30;
+//     }else if(v>10){
+//         resultLineWith = 1;
+//     }else {
+//         resultLineWith = 30-(v-0.1)/(10-0.1)*(30-1)
+//     }
+//
+//
+//     if(lastLineWidth==-1){
+//         return resultLineWith;
+//     }
+//
+//     return lastLineWidth*2/3+ resultLineWith*1/3;
+// }
+
+function calDistance(loc1, loc2) {
+    return Math.sqrt((loc1.x-loc2.x)*(loc1.x-loc2.x) + (loc1.y-loc2.y)*(loc1.y-loc2.y));
+}
+
+
+
+//转换为canvas下的坐标，从（0,0）开始
+function windowToCanvas(x, y) {
+    var bbox = canvas.getBoundingClientRect();
+    return {
+        x:Math.round(x-bbox.left),
+        y:Math.round(y-bbox.top)
+    }
+}
+
 
 function drawGrid() {
     context.save();
